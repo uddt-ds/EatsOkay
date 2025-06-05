04c93df3b69ebaf58336cdabe8104ceb8b47b6a2
우다당탕 개발자 <uddt_ds@kakao.com>
2025-06-05 22:43:39 +0900

    Initial project
    [패키지 추가]
    - SnapKit
    - Moya, RxMoya
    - RxSwift
    - ReactorKit
    - GoogleMaps SDK
    - RxDataSources
    - Kingfisher
    
    [초기 세팅]
    - TargetVersion iOS 16.6
    - SceneDelegate 설정
    - 폴더링

---
+++ EatsOkay/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  EatsOkay
+//
+//  Created by Lee on 6/5/25.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
