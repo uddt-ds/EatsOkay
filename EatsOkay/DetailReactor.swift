1023c9c658e6fadab0c2c183f969b5b3fd9a0c8d
Hyemin Park <hmpark15@gmail.com>
2025-06-13 20:59:14 +0900

    [Feat] DetailReactor 설계

--- EatsOkay/DetailReactor.swift
+++ EatsOkay/DetailReactor.swift
@@ -1,19 +1,23 @@
 
 import ReactorKit
+import CoreLocation
+import RxSwift
 
 class DetailReactor: Reactor {
     var initialState: State = .init()
     
     enum Action {
+        case backButtonTapped
         case currentLocationButtonTapped
     }
-
+    
     enum Mutation {
-        // 위도, 경도 값을 받아서 현재 위치 State를 업데이트
+        case shouldPop(Bool)
         case setCurrentLocation(lat: Double, lon: Double)
     }
     
     struct State {
+        var shouldPop: Bool = false
         // 값이 없을 수 있기 때문에 옵셔널 타입으로 정의
         var currentLatitude: Double?
         var currentLongitute: Double?
@@ -21,15 +25,43 @@ class DetailReactor: Reactor {
     
     func mutate(action: Action) -> Observable<Mutation> {
         switch action {
+        case .backButtonTapped:
+            return .just(.shouldPop(true))
         case .currentLocationButtonTapped:
-            
+            return getCurrentLocation()
+                .flatMap { coordinate -> Observable<Mutation> in
+                    // 위도, 경도로 시/도 역지오코딩
+                    return NetworkManager.shared.fetchGeoCoding(lat: coordinate.latitude, lon: coordinate.longitude)
+                        // single 타입을 Observable로 변환
+                        .asObservable()
+                        .map { addressTuple -> Mutation in
+                            let address = addressTuple.map { "($0.0) \($0.1)" } ?? "알 수 없는 위치"
+                            let location = UserDeafaultsManager.UserLocation(address: address, lat: coordinate.latitude, lon: coordinate.longitude)
+                            // UserDeafaultsManager에 위치 저장
+                            UserDeafaultsManager.shared.saveLocation(location: location)
+                            return .setCurrentLocation(lat: coordinate.latitude, lon: coordinate.longitude)
+                        }
+                }
         }
     }
     
     func reduce(state: State, mutation: Mutation) -> State {
         var newState = state
-//        switch mutation {
-//        }
+        switch mutation {
+        case .shouldPop(let flag):
+            newState.shouldPop = flag
+        // setCurrentLocation Mutation으로 전달된 위도, 경도 값을 state에 업데이트
+        case .setCurrentLocation(lat: let lat, lon: let lon):
+            newState.currentLatitude = lat
+            newState.currentLongitute = lon
+        }
         return newState
+    }
+    
+    // CLManager에서 위도, 경도 값 받기
+    private func getCurrentLocation() -> Observable<CLLocationCoordinate2D> {
+        // mock data
+        let mockData = CLLocationCoordinate2D(latitude: 37.5171, longitude: 127.0412)
+        return Observable.just(mockData)
     }
 }
--- EatsOkay/DetailViewController.swift
+++ EatsOkay/DetailViewController.swift
@@ -2,18 +2,35 @@
 import UIKit
 import GoogleMaps
 import SnapKit
+import ReactorKit
+import RxSwift
+import RxCocoa
 
-class DetailViewController: UIViewController, GMSMapViewDelegate {
-    
+class DetailViewController: UIViewController, GMSMapViewDelegate, View {
+
+    typealias Reactor = DetailReactor
+
     // 선언 시에는 초기화 하지 않고 viewDidLoad 시 초기화
     private var mapView: GMSMapView!
+
+    var disposeBag = DisposeBag()
+    
+    private let backButton: UIBarButtonItem = {
+        let button = UIBarButtonItem(
+            image: UIImage(named: "chevronLeft"),
+            style: .plain,
+            target: nil,
+            action: nil
+            )
+        return button
+    }()
     
     private let currentLocationSearchButton: UIButton = {
         let button = UIButton()
         button.setTitle("현 위치에서 검색", for: .normal)
-        button.setTitleColor(.white, for: .normal)
-        button.titleLabel?.font = UIFont(name: "Pretendard-Regular", size: 13)
-        button.backgroundColor = UIColor.customColor(hexCode: .primary400)
+        button.setTitleColor(.customColor(hexCode: .bgColor), for: .normal)
+        button.titleLabel?.font = .customFontForBody(weight: .w400)
+        button.backgroundColor = .customColor(hexCode: .primary400)
         button.setImage(UIImage(named: "reloadButton"), for: .normal)
         button.imageView?.contentMode = .scaleAspectFill
         button.layer.cornerRadius = 16
@@ -29,14 +46,17 @@ class DetailViewController: UIViewController, GMSMapViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        self.reactor = DetailReactor()
+        
+        view.backgroundColor = .customColor(hexCode: .bgColor)
         setupMapView()
         constraintsMapView()
         
-        // title 추후 변경
+        // 임시 타이틀 → 추후 UI 변경 및 키워드 기반으로 바인딩 예정
         self.title = "퇴근 후, 혼술 타임"
         
-        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "chevron.backward"), style: .plain, target: self, action: #selector(backButtonTapped))
+        navigationItem.leftBarButtonItem = backButton
+        backButton.tintColor = .customColor(hexCode: .neutral950)
     }
     
     private func setupMapView() {
@@ -64,7 +84,7 @@ class DetailViewController: UIViewController, GMSMapViewDelegate {
         mapView.snp.makeConstraints{
             $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
             $0.horizontalEdges.equalToSuperview()
-            $0.height.equalTo(250)
+            $0.height.equalTo(262)
         }
         
         currentLocationSearchButton.snp.makeConstraints{
@@ -79,11 +99,50 @@ class DetailViewController: UIViewController, GMSMapViewDelegate {
             $0.width.height.equalTo(32)
         }
     }
-    
-    // 뒤로가기 버튼 액션
-    @objc
-    private func backButtonTapped() {
-        self.navigationController?.popViewController(animated: true)
+}
+
+extension DetailViewController {
+    // reactor와 view 연결
+    func bind(reactor: DetailReactor) {
+        bindAction(reactor: reactor)
+        bindState(reactor: reactor)
     }
     
+    func bindAction(reactor: DetailReactor) {
+        backButton.rx.tap
+            .map { Reactor.Action.backButtonTapped }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
+        currentLocationButton.rx.tap
+            .map { Reactor.Action.currentLocationButtonTapped }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+    }
+    
+    func bindState(reactor: DetailReactor) {
+        reactor.state
+            .map { $0.shouldPop }
+            .distinctUntilChanged()
+            .filter { $0 }
+            .withUnretained(self)
+            .subscribe(onNext: { owner, _ in
+                owner.navigationController?.popViewController(animated: true)
+            })
+            .disposed(by: disposeBag)
+        
+        reactor.state
+            // currentLatitude, currentLongitute 값이 둘 다 존재할 경우 tuble로 return
+            .compactMap { state -> (Double, Double)? in
+                guard let lat = state.currentLatitude,
+                      let lon = state.currentLongitute else { return nil }
+                return (lat, lon)
+            }
+            .withUnretained(self)
+            .subscribe(onNext: { owner, coordinate in
+                let camera = GMSCameraPosition.camera(withLatitude: coordinate.0, longitude: coordinate.1, zoom: 15)
+                owner.mapView.animate(to: camera)
+            })
+            .disposed(by: disposeBag)
+    }
 }
