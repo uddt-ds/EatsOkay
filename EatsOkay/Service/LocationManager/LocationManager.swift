76f05b9788ab0d411bc9881c94857bdeff0f4783
LCH-1228 <chanho1228@gmail.com>
2025-06-17 16:52:04 +0900

    [Refactor] #37 - LocationManager 코드 개선
    - getCurrentLocationOnce를 methodInvoked로 수정 및 예외처리 로직을 하나의 Observable로 처리

--- EatsOkay/Service/LocationManager/LocationManager.swift
+++ EatsOkay/Service/LocationManager/LocationManager.swift
@@ -11,80 +11,55 @@ import RxSwift
 import RxRelay
 import RxCocoa
 
+extension CLLocationManager: @retroactive HasDelegate {
+    public typealias Delegate = CLLocationManagerDelegate
+}
+
 final class RxCoreLocationManagerDelegateProxy: DelegateProxy<CLLocationManager, CLLocationManagerDelegate>, DelegateProxyType, CLLocationManagerDelegate {
     
-    fileprivate let locationRelay = PublishRelay<[CLLocation]>()
-    fileprivate let errorRelay = PublishRelay<Error>()
+    weak private(set) var locationManager: CLLocationManager?
+    
+    init(locationManager: CLLocationManager) {
+        self.locationManager = locationManager
+        super.init(
+            parentObject: locationManager,
+            delegateProxy: RxCoreLocationManagerDelegateProxy.self
+        )
+    }
     
     static func registerKnownImplementations() {
         self.register { parent in
-            return RxCoreLocationManagerDelegateProxy(
-                parentObject: parent,
-                delegateProxy: self
-            )
+            RxCoreLocationManagerDelegateProxy(locationManager: parent)
         }
-    }
-    
-    static func currentDelegate(for object: CLLocationManager) -> (any CLLocationManagerDelegate)? {
-        return object.delegate
-    }
-    
-    static func setCurrentDelegate(_ delegate: (any CLLocationManagerDelegate)?, to object: CLLocationManager) {
-        object.delegate = delegate
-    }
-    
-    // TODO: 위치 요청 하여 정상적으로 Relay로 전달하고 위치서비스를 끄면 이전에 업데이트 된 값이 방출되는 오류가 있음
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if let latest = locations.last, abs(latest.timestamp.timeIntervalSinceNow) < 5 {
-            locationRelay.accept(locations)
-        } else {
-            errorRelay.accept(LocationManagerError.noLastestLocationFound)
-        }
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didFailWithError error: any Error) {
-        errorRelay.accept(error)
     }
 }
 
 extension Reactive where Base: CLLocationManager {
-    var delegate: RxCoreLocationManagerDelegateProxy {
+
+    var delegate: DelegateProxy<CLLocationManager, CLLocationManagerDelegate> {
         return RxCoreLocationManagerDelegateProxy.proxy(for: base)
     }
-    
-    var getCurrentLocationOnce: Single<(lat: Double, lon: Double)> {
-        return Single.create { single in
-            var isEmitted = false
+
+    var getCurrentLocationOnce: Observable<(lat: Double, lon: Double)> {
+        base.startUpdatingLocation()
+        return delegate.methodInvoked(
+            #selector(CLLocationManagerDelegate.locationManager(_:didUpdateLocations:))
+        )
+        .take(1)
+        .compactMap { params in
+            base.stopUpdatingLocation()
+            let locationArray = params[1] as? [CLLocation]
+            guard let coord = locationArray?.first else {
+                throw LocationManagerError.noLocationFound
+            }
             
-            let resultDisposable = Observable.merge(
-                delegate.locationRelay.map { Result<[CLLocation],Error>.success($0) },
-                delegate.errorRelay.map { Result<[CLLocation],Error>.failure($0) }
-            )
-                .take(1)
-                .subscribe(onNext: { result in
-                    guard !isEmitted else { return }
-                    isEmitted = true
-                    
-                    base.stopUpdatingLocation()
-                    
-                    switch result {
-                    case .success(let locations):
-                        if let coordinate = locations.first?.coordinate {
-                            single(.success((lat: coordinate.latitude, lon: coordinate.longitude)))
-                        } else {
-                            single(.failure(LocationManagerError.noLocationFound))
-                        }
-                    case .failure:
-                        single(.failure(LocationManagerError.locationServiceTurnedOffOrTemporaryError))
-                    }
-                })
-            
-            base.startUpdatingLocation()
-            
-            return Disposables.create {
-                resultDisposable.dispose()
-                base.stopUpdatingLocation()
+            if coord.timestamp.timeIntervalSinceNow > -5 && coord.timestamp.timeIntervalSinceNow < 0  {
+                return (lat: coord.coordinate.latitude, lon: coord.coordinate.longitude)
+            } else {
+                throw LocationManagerError.noLastestLocationFound
             }
         }
+        .share()
     }
 }
+
