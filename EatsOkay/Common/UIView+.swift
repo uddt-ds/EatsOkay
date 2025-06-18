d5a98b1062f0053a0ce3f10b0d606d5209a88b9e
LCH-1228 <chanho1228@gmail.com>
2025-06-19 01:43:36 +0900

    [feat] #81 - ToastMessage 기본 형태 구현

---
+++ EatsOkay/Common/UIView+.swift
@@ -0,0 +1,58 @@
+//
+//  UIView+.swift
+//  EatsOkay
+//
+//  Created by LCH on 6/19/25.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func showToast(message: String, alpha: CGFloat) {
+        
+        let imageView = UIImageView(image: .alertCircle)
+        imageView.contentMode = .scaleAspectFit
+        
+        let label = UILabel()
+        label.attributedText = AttributedStringManager.configureString(
+            text: message,
+            font: .customFontForBody(weight: .w400),
+            color: .bgColor,
+            alignment: .center
+        )
+        
+        let stack = UIStackView(arrangedSubviews: [imageView, label])
+        stack.spacing = 8
+        stack.alignment = .center
+        stack.isLayoutMarginsRelativeArrangement = true
+        stack.layoutMargins = UIEdgeInsets(top: 10, left: 12, bottom: 10, right: 12)
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.alpha = 0.0
+        stack.layer.cornerRadius = 12
+        stack.clipsToBounds = true
+        stack.backgroundColor = .customColor(hexCode: .neutral950).withAlphaComponent(alpha)
+        
+        self.addSubview(stack)
+        
+        stack.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalTo(self.safeAreaLayoutGuide.snp.bottom).inset(80)
+        }
+        
+        imageView.snp.makeConstraints {
+            $0.size.equalTo(16)
+        }
+        
+        UIView.animate(withDuration: 0.5, delay: 0.0, options: .curveEaseIn, animations: {
+            stack.alpha = 1.0
+        }, completion: { _ in
+            
+            UIView.animate(withDuration: 0.5, delay: 2.0, options: .curveEaseOut, animations: {
+                stack.alpha = 0.0
+            }, completion: { _ in
+                stack.removeFromSuperview()
+            })
+        })
+    }
+}
