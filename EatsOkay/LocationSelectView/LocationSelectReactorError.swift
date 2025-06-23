ee438ebc7555b1499fa5b4f255fba0ec26191577
LCH-1228 <chanho1228@gmail.com>
2025-06-23 17:11:26 +0900

    [Refactor] #111 - LocationSelectView 관련 불필요한 에러 삭제

--- EatsOkay/LocationSelectView/LocationSelectReactorError.swift
+++ EatsOkay/LocationSelectView/LocationSelectReactorError.swift
@@ -8,15 +8,12 @@
 import Foundation
 
 enum LocationSelectReactorError: Error {
-    case locationNotFoundInUserDefaults
     case locationOutsideKorea
 }
 
 extension LocationSelectReactorError: LocalizedError {
     var errorDescription: String? {
         switch self {
-        case .locationNotFoundInUserDefaults:
-            return "저장된 위치 정보를 찾을 수 없습니다."
         case .locationOutsideKorea:
             return "현재 위치는 대한민국이 아닙니다."
         }
--- EatsOkay/Service/LocationManager/LocationManagerError.swift
+++ EatsOkay/Service/LocationManager/LocationManagerError.swift
@@ -10,7 +10,6 @@ import Foundation
 enum LocationManagerError: Error {
     case noLocationFound
     case noLastestLocationFound
-    case locationServiceTurnedOffOrTemporaryError
 }
 
 extension LocationManagerError: LocalizedError {
@@ -20,13 +19,6 @@ extension LocationManagerError: LocalizedError {
             return "위치를 찾을 수 없습니다"
         case .noLastestLocationFound:
             return "최근 위치 정보를 찾을 수 없습니다."
-        case .locationServiceTurnedOffOrTemporaryError:
-            return """
-                    위치 서비스를 사용할 수 없습니다.
-                    기기의 위치 서비스가 꺼져 있거나,
-                    일시적인 문제로 인해 위치 정보를 가져올 수 없습니다.
-                    설정을 확인하거나 잠시 후 다시 시도해주세요.
-                    """
         }
     }
 }
